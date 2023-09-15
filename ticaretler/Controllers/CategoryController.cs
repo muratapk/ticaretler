79352f3fff97ee826c3cf30ca352c055486fae7b
muratapk <muratapk@hotmail.com>
2023-09-15 19:17:56 +0300

    Category Elendi

---
+++ ticaretler/Controllers/CategoryController.cs
@@ -0,0 +1,16 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ticaretler.Controllers
+{
+    public class CategoryController : Controller
+    {
+        CategoryManager cm = new CategoryManager(new EfCategoryRepository());
+        public IActionResult Index()
+        {
+            var getir=cm.TGetAll();
+            return View(getir);
+        }
+    }
+}
